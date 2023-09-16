3508cd8d9b6ac1578e5ec4aa01886d601110298c
Krishnasis Mandal <krishnasismandal97@gmail.com>
2023-09-16 22:13:03 +0200

    We need to find multiple instances of a word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from hotpdf.processor import get_document_xml
 from hotpdf.memory_map import MemoryMap
+from hotpdf.utils import filter_adjacent_coords
 
 if __name__ == "__main__":
     document_xml = get_document_xml("test.pdf")
@@ -10,5 +11,42 @@ if __name__ == "__main__":
     memory_map.load_memory_map([document_xml[0]])
     memory_map.display_memory_map(save=True)
 
-    print(memory_map.extract_text_from_bbox(0, 85, 0, 50))
-    print(memory_map.find_text("Interest"))
+    print(memory_map.extract_text_from_bbox(70, 76, 546, 560))
+    # print(memory_map.find_text("Interest"))
+    print(filter_adjacent_coords(*memory_map.find_text("UBER")))
+    # (['U', 'B', 'E', 'R'], [{'x': 189, 'y': 52}, {'x': 189, 'y': 52}, {'x': 441, 'y': 82}, {'x': 441, 'y': 82}])
+    coords = [
+        [
+            [
+                {"x": 70, "y": 546},
+                {"x": 346, "y": 300},
+                {"x": 324, "y": 287},
+                {"x": 356, "y": 287},
+            ]
+        ],
+        [
+            [
+                {"x": 70, "y": 546},
+                {"x": 346, "y": 300},
+                {"x": 324, "y": 287},
+                {"x": 356, "y": 287},
+            ]
+        ],
+        [
+            [
+                {"x": 76, "y": 560},
+                {"x": 94, "y": 546},
+                {"x": 170, "y": 348},
+                {"x": 308, "y": 317},
+            ]
+        ],
+        [
+            [
+                {"x": 76, "y": 560},
+                {"x": 94, "y": 546},
+                {"x": 170, "y": 348},
+                {"x": 308, "y": 317},
+            ]
+        ],
+    ]
+    print(memory_map.extract_text_from_bbox(70, 76, 546, 547))
