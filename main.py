5bd3e604a22b67d3fa58e1c01defdd6c2275cbb6
Krishnasis Mandal <krishnasis.mandal@prestatech.com>
2023-09-16 23:52:55 +0200

    Mutliple text instance search

--- main.py
+++ main.py
@@ -10,43 +10,10 @@ if __name__ == "__main__":
     memory_map.build_memory_map()
     memory_map.load_memory_map([document_xml[0]])
     memory_map.display_memory_map(save=True)
+    print(memory_map.extract_text_from_bbox(306, 345, 531, 532))
 
-    print(memory_map.extract_text_from_bbox(70, 76, 546, 560))
-    # print(memory_map.find_text("Interest"))
-    print(filter_adjacent_coords(*memory_map.find_text("UBER")))
-    # (['U', 'B', 'E', 'R'], [{'x': 189, 'y': 52}, {'x': 189, 'y': 52}, {'x': 441, 'y': 82}, {'x': 441, 'y': 82}])
-    coords = [
-        [
-            [
-                {"x": 70, "y": 546},
-                {"x": 346, "y": 300},
-                {"x": 324, "y": 287},
-                {"x": 356, "y": 287},
-            ]
-        ],
-        [
-            [
-                {"x": 70, "y": 546},
-                {"x": 346, "y": 300},
-                {"x": 324, "y": 287},
-                {"x": 356, "y": 287},
-            ]
-        ],
-        [
-            [
-                {"x": 76, "y": 560},
-                {"x": 94, "y": 546},
-                {"x": 170, "y": 348},
-                {"x": 308, "y": 317},
-            ]
-        ],
-        [
-            [
-                {"x": 76, "y": 560},
-                {"x": 94, "y": 546},
-                {"x": 170, "y": 348},
-                {"x": 308, "y": 317},
-            ]
-        ],
-    ]
-    print(memory_map.extract_text_from_bbox(70, 76, 546, 547))
+    coords = filter_adjacent_coords(*memory_map.find_text("TERRAGNI"))
+    for coord in coords:
+        print()
+        print(coord)
+    # print(memory_map.find_text("TERRAGNI"))
