62e271679973173c1a9db815173b2276ed3ba1fb
Izel Odabasi <Izel.Odabasi@prestacap.com>
2023-12-04 13:58:55 +0000

    Find text based on neighbour char

--- hotpdf/utils.py
+++ hotpdf/utils.py
@@ -1,22 +1,22 @@
 from copy import deepcopy
 
 
-def find_neighbour_coord(anchor_coord, coords_array, max_distance):
+def find_neighbour_coord(pre_neighbour_coord, coords_array, max_distance=6):
     """
-    Find a neighboring coordinate within a specified maximum distance.
+    Find a neighbouring coordinate within a specified maximum distance.
 
     Args:
-        anchor_coord (dict): The anchor (start) coordinate.
+        pre_neighbour_coord (dict): Previous char coordinate.
         coords_array (list): List of coordinates to search.
-        max_distance (int): The maximum distance allowed from anchor.
 
     Returns:
-        dict: The neighboring coordinate if found, else None.
+        dict: The neighbouring coordinate if found, else None.
     """
     for coord in coords_array:
         if (
-            0 <= (coord["x"] - anchor_coord["x"]) <= max_distance + 5
-            and anchor_coord["y"] == coord["y"]
+            0 <= (coord["x"] - pre_neighbour_coord["x"]) <= max_distance
+            and coord["x"] >= pre_neighbour_coord["x_end"]
+            and pre_neighbour_coord["y"] == coord["y"]
         ):
             return coord
 
@@ -40,12 +40,14 @@ def filter_adjacent_coords(text, coords):
     anchor_coords = coords[0]
     for anchor_coord in anchor_coords:
         neighbours = [anchor_coord]
+        pre_neighbour_coord = anchor_coord
         for idx, coords_j in enumerate(coords[1:]):
             neighbour_coord = find_neighbour_coord(
-                anchor_coord, coords_j, ((idx + 1) * 6)
+                pre_neighbour_coord, coords_j
             )
             if neighbour_coord:
                 neighbours.append(neighbour_coord)
+                pre_neighbour_coord = neighbour_coord
         if len(neighbours) == max_len:
             adjacent_groups.append(deepcopy(neighbours))
         neighbours = []
