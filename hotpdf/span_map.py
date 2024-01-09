9d6bc8f3c510a2ef095de4a0d4fb92e6093c86e2
Mattia Callegari <mattia.callegari@prestatech.com>
2024-01-09 22:15:15 +0100

    fix: change map attribute in span_map to non-keyword name

--- hotpdf/span_map.py
+++ hotpdf/span_map.py
@@ -10,10 +10,10 @@ class SpanMap:
     """
 
     def __init__(self) -> None:
-        self.map: dict[str, Span] = dict()
+        self.span_map: dict[str, Span] = dict()
 
     def __len__(self) -> int:
-        return len(self.map)
+        return len(self.span_map)
 
     def __getitem__(self, span_id: str) -> Union[Span, None]:
         return self.get_span(span_id)
@@ -24,13 +24,13 @@ class SpanMap:
         self.insert(span_id, hot_character)
 
     def items(self) -> Iterable[tuple[str, Span]]:
-        return self.map.items()
+        return self.span_map.items()
 
     def insert(self, span_id: Union[str, None], hot_character: HotCharacter) -> None:
         if not span_id:
             raise KeyError("Cannot set key as None")
-        if span_id in self.map:
-            span = self.map[span_id]
+        if span_id in self.span_map:
+            span = self.span_map[span_id]
             assert span.x0 <= hot_character.x
             assert span.y0 == hot_character.y
             span.characters.append(hot_character)
@@ -45,9 +45,9 @@ class SpanMap:
                 span_id=span_id,
             )
 
-        self.map[span_id] = span
+        self.span_map[span_id] = span
 
     def get_span(self, span_id: Union[str, None]) -> Union[Span, None]:
         if span_id:
-            return self.map.get(span_id)
+            return self.span_map.get(span_id)
         return None
