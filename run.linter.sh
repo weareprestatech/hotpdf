908a75fe2b2ea6a157960d3f3721055b42fe705e
Mattia Callegari <mattia.callegari@mail.polimi.it>
2024-01-14 17:44:26 +0100

    chore: cleaning up

--- run.linter.sh
+++
@@ -1,4 +0,0 @@
-set -e
-python -m pipenv run mypy hotpdf/ --check-untyped-defs --strict
-python -m pipenv run mypy tests/ --check-untyped-defs
-python -m pipenv run flake8 hotpdf/ --ignore=E501,W503
