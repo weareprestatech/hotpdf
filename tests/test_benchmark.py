f4287a7f63c9ce357f51d0ef8925594c2576f0f3
Krishnasis <krishnasis.mandal@prestatech.com>
2024-01-16 16:37:02 +0530

    Add benchmark tests (#35)
    * test: Add speed benchmark test
    
    * Update max time in benchmark tests to support small runner specs
    
    * test: add memory benchmark test
    
    * Add small test file
    
    * test: refactor to split operations
    
    * chore: send seconds as param

---
+++ tests/test_benchmark.py
@@ -0,0 +1,62 @@
+import time
+import tracemalloc
+
+import pytest
+
+from hotpdf import HotPdf
+
+
+@pytest.fixture
+def default_file_name():
+    return "tests/resources/PDF.pdf"
+
+
+@pytest.fixture
+def multiple_pages_file_name():
+    return "tests/resources/20pages.pdf"
+
+
+@pytest.fixture
+def luca_mock_file_name():
+    return "tests/resources/luca_mock.pdf"
+
+
+def perform_speed_test(file_name, expected_processing_seconds):
+    start_time = time.time()
+    hot_pdf_object = HotPdf()
+    hot_pdf_object.load(file_name)
+    end_time = time.time()
+    assert (end_time - start_time) < expected_processing_seconds, "Benchmark time exceeded!"
+
+
+def perform_memory_test(file_name, expected_peak_memory):
+    tracemalloc.start()
+    hot_pdf_object = HotPdf()
+    hot_pdf_object.load(file_name)
+    peak_memory = tracemalloc.get_traced_memory()[1] / (1024 * 1024)
+    tracemalloc.stop()
+    assert peak_memory < expected_peak_memory, "Benchmark memory usage exceeded!"
+
+
+def test_speed_benchmark_multiple_pages(multiple_pages_file_name):
+    perform_speed_test(multiple_pages_file_name, 3.5)
+
+
+def test_memory_benchmark_multiple_pages(multiple_pages_file_name):
+    perform_memory_test(multiple_pages_file_name, 15)
+
+
+def test_speed_luca_mock(luca_mock_file_name):
+    perform_speed_test(luca_mock_file_name, 2.5)
+
+
+def test_memory_luca_mock(luca_mock_file_name):
+    perform_memory_test(luca_mock_file_name, 12)
+
+
+def test_speed_default_file(default_file_name):
+    perform_speed_test(default_file_name, 2)
+
+
+def test_memory_default_file(default_file_name):
+    perform_memory_test(default_file_name, 0.6)
