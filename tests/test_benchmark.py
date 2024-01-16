124bfd68ac672ca97f9fd1f71cab8c1265754f72
Krishnasis Mandal <krishnasis.mandal@prestatech.com>
2024-01-16 16:29:44 +0530

    test: refactor to split operations

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -21,37 +21,42 @@ def luca_mock_file_name():
     return "tests/resources/luca_mock.pdf"
 
 
-def test_benchmark_multiple_pages(multiple_pages_file_name):
+def perform_speed_test(file_name):
     start_time = time.time()
+    hot_pdf_object = HotPdf()
+    hot_pdf_object.load(file_name)
+    end_time = time.time()
+    assert (end_time - start_time) < 2, "Benchmark time exceeded!"
+
+
+def perform_memory_test(file_name, expected_peak_memory):
     tracemalloc.start()
     hot_pdf_object = HotPdf()
-    hot_pdf_object.load(multiple_pages_file_name)
-    end_time = time.time()
+    hot_pdf_object.load(file_name)
     peak_memory = tracemalloc.get_traced_memory()[1] / (1024 * 1024)
     tracemalloc.stop()
-    assert (end_time - start_time) < 2, "Benchmark time exceeded!"
-    assert peak_memory < 15, "Benchmark memory usage exceeded!"
+    assert peak_memory < expected_peak_memory, "Benchmark memory usage exceeded!"
 
 
-def test_luca_mock(luca_mock_file_name):
-    start_time = time.time()
-    tracemalloc.start()
-    hot_pdf_object = HotPdf()
-    hot_pdf_object.load(luca_mock_file_name)
-    end_time = time.time()
-    peak_memory = tracemalloc.get_traced_memory()[1] / (1024 * 1024)
-    tracemalloc.stop()
-    assert (end_time - start_time) < 2, "Benchmark time exceeded!"
-    assert peak_memory < 12, "Benchmark memory usage exceeded!"
+def test_speed_benchmark_multiple_pages(multiple_pages_file_name):
+    perform_speed_test(multiple_pages_file_name)
 
 
-def test_default_file(default_file_name):
-    start_time = time.time()
-    tracemalloc.start()
-    hot_pdf_object = HotPdf()
-    hot_pdf_object.load(default_file_name)
-    end_time = time.time()
-    peak_memory = tracemalloc.get_traced_memory()[1] / (1024 * 1024)
-    tracemalloc.stop()
-    assert (end_time - start_time) < 2, "Benchmark time exceeded!"
-    assert peak_memory < 0.6, "Benchmark memory usage exceeded!"
+def test_memory_benchmark_multiple_pages(multiple_pages_file_name):
+    perform_memory_test(multiple_pages_file_name, 15)
+
+
+def test_speed_luca_mock(luca_mock_file_name):
+    perform_speed_test(luca_mock_file_name)
+
+
+def test_memory_luca_mock(luca_mock_file_name):
+    perform_memory_test(luca_mock_file_name, 12)
+
+
+def test_speed_default_file(default_file_name):
+    perform_speed_test(default_file_name)
+
+
+def test_memory_default_file(default_file_name):
+    perform_memory_test(default_file_name, 0.6)
