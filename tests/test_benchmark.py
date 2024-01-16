1a7270a6e73214b1143a3a94ebffc4e483806268
Krishnasis Mandal <krishnasis.mandal@prestatech.com>
2024-01-16 05:53:33 +0530

    Update max time in benchmark tests to support small runner specs

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -19,11 +19,11 @@ def test_benchmark_multiple_pages(multiple_pages_file_name):
     start_time = time.time()
     hot_pdf_object = HotPdf()
     hot_pdf_object.load(multiple_pages_file_name)
-    assert (time.time() - start_time) < 0.9, "Benchmark time exceeded!"
+    assert (time.time() - start_time) < 2, "Benchmark time exceeded!"
 
 
 def test_luca_mock(luca_mock_file_name):
     start_time = time.time()
     hot_pdf_object = HotPdf()
     hot_pdf_object.load(luca_mock_file_name)
-    assert (time.time() - start_time) < 0.9, "Benchmark time exceeded!"
+    assert (time.time() - start_time) < 3, "Benchmark time exceeded!"
