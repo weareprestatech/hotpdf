5b8076cba02b0a67f7203b881be69623cdf6a336
Krishnasis Mandal <krishnasis.mandal@prestatech.com>
2024-01-30 00:33:27 +0530

    test: increase thresholds

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -26,23 +26,23 @@ def perform_memory_test(file_name, expected_peak_memory):
 
 
 def test_speed_benchmark_multiple_pages(multiple_pages_file_name):
-    perform_speed_test(multiple_pages_file_name, 2)
+    perform_speed_test(multiple_pages_file_name, 2.5)
 
 
 def test_memory_benchmark_multiple_pages(multiple_pages_file_name):
-    perform_memory_test(multiple_pages_file_name, 15.5)
+    perform_memory_test(multiple_pages_file_name, 16)
 
 
 def test_speed_luca_mock(mock_hotpdf_bank_file_name):
-    perform_speed_test(mock_hotpdf_bank_file_name, 3)
+    perform_speed_test(mock_hotpdf_bank_file_name, 3.5)
 
 
 def test_memory_luca_mock(mock_hotpdf_bank_file_name):
-    perform_memory_test(mock_hotpdf_bank_file_name, 12.5)
+    perform_memory_test(mock_hotpdf_bank_file_name, 12.99)
 
 
 def test_speed_default_file(valid_file_name):
-    perform_speed_test(valid_file_name, 2.5)
+    perform_speed_test(valid_file_name, 2.9)
 
 
 @pytest.mark.skip(reason="Need to perform benchmarks first")
