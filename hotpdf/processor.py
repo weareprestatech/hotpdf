cd0c03e75acfbe7981d10cf550262a6b44713efd
izelodabasi <izel.odabasi@prestatech.com>
2024-02-07 13:42:11 +0100

    drop to pdfminer

--- hotpdf/processor.py
+++ hotpdf/processor.py
@@ -3,8 +3,12 @@ from io import IOBase
 from pathlib import PurePath
 from typing import Optional, Union
 
-from pdfminer.high_level import extract_pages
+from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
 
 from hotpdf.memory_map import MemoryMap
 
@@ -21,16 +25,27 @@ def __process(
     pages: list[MemoryMap] = []
     page_numbers = sorted(page_numbers) if page_numbers else []
 
-    laparams_obj = LAParams(**laparams if laparams else {})  # type: ignore
+    file = open(source, "rb") if not isinstance(source, IOBase) else source  # noqa: SIM115
 
-    hl_page_layouts = extract_pages(
-        source, password=password, page_numbers=page_numbers, caching=False, laparams=laparams_obj
-    )
-    for page_layout in hl_page_layouts:
-        parsed_page: MemoryMap = MemoryMap()
-        parsed_page.build_memory_map()
-        parsed_page.load_memory_map(page=page_layout, include_annotation_spaces=include_annotation_spaces)
-        pages.append(parsed_page)
+    laparams_obj = LAParams(**laparams if laparams else {})
+    parser = PDFParser(file)
+    encoded_password = password.encode() if password else b""
+    doc = PDFDocument(parser, password=encoded_password)
+    rsrcmgr = PDFResourceManager()
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams_obj)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+    for page_number, page in enumerate(PDFPage.create_pages(doc), start=0):
+        if not page_numbers or page_number in page_numbers:
+            interpreter.process_page(page)
+            page_layout = device.get_result()
+            parsed_page: MemoryMap = MemoryMap()
+            parsed_page.build_memory_map()
+            parsed_page.load_memory_map(page=page_layout, include_annotation_spaces=include_annotation_spaces)
+            pages.append(parsed_page)
+
+    if not isinstance(source, IOBase):
+        file.close()
     return pages
 
 
